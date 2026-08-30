ecdee0193d48681638e2b7142bd3cdf29a659c62
ViniciusZamoraFernandes <viniciuszamoragithub@gmail.com>
2026-08-29 23:28:07 -0300

    criei a tela de turmas

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,180 +4,141 @@ import { usePathname, useRouter } from 'next/navigation';
 
 import styles from '../../styles/NavBar.module.css';
 
-import {
-  Bell,
-  House,
-  DoorClosed,
-  List,
-  ScrollText,
-  Cog,
-  LogOut
-} from 'lucide-react';
+interface ItemMenu {
+    nome: string;
+    rota: string;
+    iconeBranco: string;
+    iconePreto: string;
+}
+
+const itensSuperiores: ItemMenu[] = [
+    {
+        nome: 'Notificações',
+        rota: '/notificacoes',
+        iconeBranco: '/icons/notificacoesBranco.svg',
+        iconePreto: '/icons/notificacoesPreto.svg',
+    },
+    {
+        nome: 'Tela Inicial',
+        rota: '/',
+        iconeBranco: '/icons/telaInicialBranco.svg',
+        iconePreto: '/icons/telaInicialPreto.svg',
+    },
+    {
+        nome: 'Turmas',
+        rota: '/turmas',
+        iconeBranco: '/icons/turmaBranco.svg',
+        iconePreto: '/icons/turmaPreto.svg',
+    },
+    {
+        nome: 'Chamada',
+        rota: '/chamada',
+        iconeBranco: '/icons/chamadaBranco.svg',
+        iconePreto: '/icons/chamadaPreto.svg',
+    },
+    {
+        nome: 'Documentos',
+        rota: '/documentos',
+        iconeBranco: '/icons/boletimBranco.svg',
+        iconePreto: '/icons/boletimPreto.svg',
+    },
+];
+
+const configuracoes: ItemMenu = {
+    nome: 'Configurações',
+    rota: '/configuracoes',
+    iconeBranco: '/icons/configuracoesBranco.svg',
+    iconePreto: '/icons/configuracoesPreto.svg',
+};
 
 export default function NavBar() {
-  const router = useRouter();
-  const pathname = usePathname();
+    const router = useRouter();
+    const pathname = usePathname();
 
-  const estaSelecionado = (rota: string) => {
-    if (rota === '/') {
-      return pathname === '/';
-    }
+    const estaSelecionado = (rota: string) => {
+        if (rota === '/') {
+            return pathname === '/';
+        }
 
-    return pathname.startsWith(rota);
-  };
+        return pathname.startsWith(rota);
+    };
 
-  return (
-    <div className={styles.container}>
+    return (
+        <div className={styles.container}>
+            <div className={styles.barraSuperior}>
+                {itensSuperiores.map((item) => {
+                    const selecionado = estaSelecionado(item.rota);
 
-      <div className={styles.barraSuperior}>
+                    return (
+                        <div
+                            key={item.rota}
+                            className={`${styles.itemBarraLateral} ${
+                                selecionado ? styles.itemSelecionado : ''
+                            }`}
+                            onClick={() => router.push(item.rota)}
+                        >
+                            <div className={styles.iconeItemBarraLateral}>
+                                <img
+                                    src={
+                                        selecionado
+                                            ? item.iconePreto
+                                            : item.iconeBranco
+                                    }
+                                    alt=""
+                                    className={styles.iconeMenu}
+                                />
+                            </div>
 
-        <div
-          className={`${styles.itemBarraLateral} ${
-            estaSelecionado('/notificacoes')
-              ? styles.itemSelecionado
-              : ''
-          }`}
-          onClick={() => router.push('/notificacoes')}
-        >
-          <div className={styles.iconeItemBarraLateral}>
-            <Bell
-              size={42}
-              strokeWidth={2}
-            />
-          </div>
+                            <div className={styles.nomeItemBarraLateral}>
+                                <span>{item.nome}</span>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
 
-          <div className={styles.nomeItemBarraLateral}>
-            <span>Notificações</span>
-          </div>
+            <div className={styles.barraInferior}>
+                <div
+                    className={`${styles.itemBarraLateral} ${
+                        estaSelecionado(configuracoes.rota)
+                            ? styles.itemSelecionado
+                            : ''
+                    }`}
+                    onClick={() => router.push(configuracoes.rota)}
+                >
+                    <div className={styles.iconeItemBarraLateral}>
+                        <img
+                            src={
+                                estaSelecionado(configuracoes.rota)
+                                    ? configuracoes.iconePreto
+                                    : configuracoes.iconeBranco
+                            }
+                            alt=""
+                            className={styles.iconeMenu}
+                        />
+                    </div>
+
+                    <div className={styles.nomeItemBarraLateral}>
+                        <span>{configuracoes.nome}</span>
+                    </div>
+                </div>
+
+                <div
+                    className={`${styles.itemBarraLateral} ${styles.itemSair}`}
+                >
+                    <div className={styles.iconeItemBarraLateral}>
+                        <img
+                            src="/icons/sairBranco.svg"
+                            alt=""
+                            className={styles.iconeMenu}
+                        />
+                    </div>
+
+                    <div className={styles.nomeItemBarraLateral}>
+                        <span>Sair</span>
+                    </div>
+                </div>
+            </div>
         </div>
-
-
-        <div
-          className={`${styles.itemBarraLateral} ${
-            estaSelecionado('/')
-              ? styles.itemSelecionado
-              : ''
-          }`}
-          onClick={() => router.push('/')}
-        >
-          <div className={styles.iconeItemBarraLateral}>
-            <House
-              size={42}
-              strokeWidth={2}
-            />
-          </div>
-
-          <div className={styles.nomeItemBarraLateral}>
-            <span>Tela Inicial</span>
-          </div>
-        </div>
-
-
-        <div
-          className={`${styles.itemBarraLateral} ${
-            estaSelecionado('/turmas')
-              ? styles.itemSelecionado
-              : ''
-          }`}
-          onClick={() => router.push('/turmas')}
-        >
-          <div className={styles.iconeItemBarraLateral}>
-            <DoorClosed
-              size={42}
-              strokeWidth={2}
-            />
-          </div>
-
-          <div className={styles.nomeItemBarraLateral}>
-            <span>Turmas</span>
-          </div>
-        </div>
-
-
-        <div
-          className={`${styles.itemBarraLateral} ${
-            estaSelecionado('/chamada')
-              ? styles.itemSelecionado
-              : ''
-          }`}
-          onClick={() => router.push('/chamada')}
-        >
-          <div className={styles.iconeItemBarraLateral}>
-            <List
-              size={42}
-              strokeWidth={2}
-            />
-          </div>
-
-          <div className={styles.nomeItemBarraLateral}>
-            <span>Chamada</span>
-          </div>
-        </div>
-
-
-        <div
-          className={`${styles.itemBarraLateral} ${
-            estaSelecionado('/documentos')
-              ? styles.itemSelecionado
-              : ''
-          }`}
-          onClick={() => router.push('/documentos')}
-        >
-          <div className={styles.iconeItemBarraLateral}>
-            <ScrollText
-              size={42}
-              strokeWidth={2}
-            />
-          </div>
-
-          <div className={styles.nomeItemBarraLateral}>
-            <span>Documentos</span>
-          </div>
-        </div>
-
-      </div>
-
-
-      <div className={styles.barraInferior}>
-
-        <div
-          className={`${styles.itemBarraLateral} ${
-            estaSelecionado('/configuracoes')
-              ? styles.itemSelecionado
-              : ''
-          }`}
-          onClick={() => router.push('/configuracoes')}
-        >
-          <div className={styles.iconeItemBarraLateral}>
-            <Cog
-              size={42}
-              strokeWidth={2}
-            />
-          </div>
-
-          <div className={styles.nomeItemBarraLateral}>
-            <span>Configurações</span>
-          </div>
-        </div>
-
-
-        <div
-          className={`${styles.itemBarraLateral} ${styles.itemSair}`}
-        >
-          <div className={styles.iconeItemBarraLateral}>
-            <LogOut
-              size={42}
-              strokeWidth={2}
-            />
-          </div>
-
-          <div className={styles.nomeItemBarraLateral}>
-            <span>Sair</span>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
-  );
+    );
 }
