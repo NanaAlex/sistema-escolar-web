6a26be74a90d319d7203ac1464a320c158db5ec4
ViniciusZamoraFernandes <viniciuszamoragithub@gmail.com>
2026-09-07 18:54:17 -0300

    Adiciona tela de perfil do professor

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,28 +1,35 @@
+import Link from 'next/link'
+import { CircleUserRound } from 'lucide-react'
 import styles from '../../styles/Header.module.css'
-import { CircleUserRound } from 'lucide-react';
 
+export default function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoHeader}>
+        <span className={styles.textoHeader}>
+          UniClass
+        </span>
+      </div>
 
-export default function Header(){
-    return(
-        <div className={styles.container}>
-            <div className={styles.logoHeader}>
-                <text className={styles.textoHeader}>UniClass</text>
-            </div>
-
-            <div className={styles.perfilHeader}>
-                <div  className={styles.nomeProfessorHeader}>
-                    <text className={styles.textoNomeProfessorHeader}>João Gomes</text>
-                </div>
-
-            <div className={styles.iconePerfilHeader}>
-                <CircleUserRound
-                    size={64}
-                    color="white"
-                    strokeWidth={2}
-                />
-            </div>
-
-            </div>
+      <div className={styles.perfilHeader}>
+        <div className={styles.nomeProfessorHeader}>
+          <span className={styles.textoNomeProfessorHeader}>
+            João Gomes
+          </span>
         </div>
-    );
+
+        <Link
+          href="/perfil"
+          className={styles.iconePerfilHeader}
+          aria-label="Abrir perfil do professor"
+        >
+          <CircleUserRound
+            size={64}
+            color="white"
+            strokeWidth={2}
+          />
+        </Link>
+      </div>
+    </div>
+  )
 }
